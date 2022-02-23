24376e7709e33a3230ad7ff0cb319af3eca26305
gustavodacrvi <gustavodacrvi@gmail.com>
2022-02-23 19:44:41 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: "#FFAD00",
+      green: "#00A78D",
+      white: '#ffffff',
+    },
+    fontFamily: {
+      body: ["Scala Sans"],
+    },
+  },
+  plugins: [],
+}
