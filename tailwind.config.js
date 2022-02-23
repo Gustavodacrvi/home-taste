c330df242c63798dc660a5ff6677a80c19263e95
gustavodacrvi <gustavodacrvi@gmail.com>
2022-02-23 20:59:13 -0300

    navbar sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "*.{html,js}"],
   theme: {
     colors: {
       primary: "#FFAD00",
       green: "#00A78D",
       white: '#ffffff',
+      "darked-background": "rgba(0, 0, 0, 0.6)",
     },
     fontFamily: {
       body: ["Scala Sans"],
