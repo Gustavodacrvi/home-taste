0d7eee04e779c3ebfff799fd5d638f2009355123
gustavodacrvi <gustavodacrvi@gmail.com>
2022-02-24 16:44:31 -0300

    footer

--- src/main.js
+++ src/main.js
@@ -5,3 +5,18 @@
 import { createApp } from './assets/petite-vue.module'
 
 createApp().mount("nav")
+
+
+/* function Footer(props) {
+  return {
+    count: props.initialCount,
+    inc() {
+      this.count++
+    },
+    mounted() {
+      console.log(`I'm mounted!`)
+    }
+  }
+}
+ */
+createApp().mount("footer")
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}", "*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./dist/**/*.{html,js}", "*.{html,js}"],
   theme: {
     colors: {
       primary: "#FFAD00",
       green: "#00A78D",
       white: '#ffffff',
+
+      'black': "#000",
       'gray-border': "#DDDDDE",
       "darked-background": "rgba(0, 0, 0, 0.6)",
     },
