9c670655879feb8b1b8a06450756eff5652d176d
gustavodacrvi <gustavodacrvi@gmail.com>
2022-02-25 15:18:45 -0300

    /dist

--- dist/bundle.js
+++ dist/bundle.js
@@ -6,6 +6,20 @@
   const { createApp } = PetiteVue;
 
   createApp().mount("nav");
+
+
+  /* function Footer(props) {
+    return {
+      count: props.initialCount,
+      inc() {
+        this.count++
+      },
+      mounted() {
+        console.log(`I'm mounted!`)
+      }
+    }
+  }
+   */
   createApp().mount("footer");
 
 }));
