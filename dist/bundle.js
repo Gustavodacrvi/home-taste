1f51855ca3210637df4bba536af8ad675f68c81d
gustavodacrvi <gustavodacrvi@gmail.com>
2022-02-25 15:17:08 -0300

    Hoe werkt Hometaste categories

--- dist/bundle.js
+++ dist/bundle.js
@@ -6,20 +6,6 @@
   const { createApp } = PetiteVue;
 
   createApp().mount("nav");
-
-
-  /* function Footer(props) {
-    return {
-      count: props.initialCount,
-      inc() {
-        this.count++
-      },
-      mounted() {
-        console.log(`I'm mounted!`)
-      }
-    }
-  }
-   */
   createApp().mount("footer");
 
 }));
